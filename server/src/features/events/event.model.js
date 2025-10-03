af29d68dcf75b0d87683fc4472b34e533f73c525
Jomana Mahmoud Tantawy <140981371+JomanaMahmoud@users.noreply.github.com>
2025-10-03 23:11:35 +0300

    fix: remove redundant id field from Event schema

--- server/src/features/events/event.model.js
+++ server/src/features/events/event.model.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 const eventSchema = new Schema(
   {
-    id: { type: String },
     name: { type: String, required: true, trim: true },
     eventType: {
       type: String,
