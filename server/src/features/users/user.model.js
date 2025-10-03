691fd20b7cc949be73e79b8eb2310f9e43ddad22
Nada Yasser <nadayasser885@gmail.com>
2025-10-03 16:27:43 +0300

    feat(users): add user model (#10)
    * feat: implement userschema for db
    
    * fix: removed attribuites and change name of attribute password

---
+++ server/src/features/users/user.model.js
@@ -0,0 +1,76 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
+
+
+     studentStaffId: {
+      type: String,
+      unique: true,
+      sparse: true,
+      required: function () {
+        return ["student", "staff", "ta", "professor"].includes(this.role);
+      },
+    },
+    
+     companyName: {
+      type: String,
+      required: function () {
+        return this.role === "vendor";
+      },
+    },
+    companyLogoUrl: { 
+    type: String, 
+    required: function () {
+       return this.role === "vendor";
+       } 
+     },
+
+    taxCardUrl: { 
+     type: String, 
+      required: function () {
+       return this.role === "vendor";
+       } 
+    },
+
+    role: {
+      type: String,
+      required: true,
+      enum: ["student", "staff", "ta", "professor", "vendor", "admin", "events_office"],
+    },
+
+    status: {
+      type: String,
+      required: true,
+      enum: ["pending", "active", "blocked"],
+      default: "pending",
+    },
+    verificationToken: { type: String },
+
+    walletBalance: { type: Number, default: 0 },
+
+    representatives: [
+      {
+        name: String,
+        email: String,
+        idCardUrl: String,
+      },
+    ],
+
+   
+    deletedAt: { type: Date, default: null },
+
+},
+  { timestamps: true }
+);
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.passwordHash);
+};
+
+export const User = mongoose.model('User', userSchema);
