e1d2c6bba506a7ea1041ea1e4f93a0b193f5cd54
walid moussa <60612554+walidmouss@users.noreply.github.com>
2025-10-04 11:30:50 +0300

    feat(applications): add applications model, controller, service, and … (#13)
    * feat(applications): add applications model, controller, service, and routes
    
    * feat/edited applications file names
    
    ---------
    
    Co-authored-by: Walid Moussa <walidmoussa@gmail.com>

---
+++ server/src/features/applications/application.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema(
+  {
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    bazaarId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    boothSize: {
+      type: String,
+      enum: ["2x2", "4x4"],
+      required: true,
+    },
+    durationWeeks: {
+      type: Number,
+      required: true,
+    },
+    locationPreference: {
+      type: String,
+    },
+    staff: [
+      {
+        name: { type: String },
+        email: { type: String },
+        id: { type: String },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+      required: true,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["unpaid", "paid"],
+      default: "unpaid",
+      required: true,
+    },
+    qrCodeUrl: {
+      type: String,
+    },
+    deletedAt: {
+      type: Date,
+      default: null, // null means application is active
+    },
+  },
+  {
+    timestamps: true, // automatically adds createdAt & updatedAt
+  }
+);
+
+module.exports = mongoose.model("Application", applicationSchema);
