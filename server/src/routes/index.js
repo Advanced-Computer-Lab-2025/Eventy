e855623d42f0c34efe9cebd59fc73aa7f11ea696
Jomana Mahmoud Tantawy <140981371+JomanaMahmoud@users.noreply.github.com>
2025-10-05 21:03:04 +0300

    feat(applications): add post endpoint in applications to apply to bazaar (#23)
    * feat: implement applyToBazaar endpoint
    
    * feat: refactor application model to use ES6 import/export syntax
    
    * feat: add application route for applying to bazaar
    
    * feat: implement ApplicationService class for application management
    
    * feat: add validation schema for applying to bazaar
    
    * fix: correct controller reference for bazaar creation route
    
    * feat: add applications route to main router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,7 @@
 
 import express from 'express';
 import eventRoutes from '../features/events/event.route.js'
+import applicationRoutes from '../features/applications/application.route.js';
 
 const PORT = process.env.PORT || 5000;
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get('/', (req, res) => {
 
 // Events routes
 router.use('/events', eventRoutes);
-
+// Applications routes
+router.use('/applications', applicationRoutes);
 export default router;
