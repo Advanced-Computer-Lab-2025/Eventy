75b208d55ac54aedd47e9b5bfc01a9c0c2de8b71
Jomana Mahmoud Tantawy <140981371+JomanaMahmoud@users.noreply.github.com>
2025-10-05 22:37:32 +0300

    feat: add GET endpoint to retrieve user's applications

--- server/src/features/applications/application.route.js
+++ server/src/features/applications/application.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { ApplicationController } from "./application.controller.js";
-import { applyToBazaarSchema } from "./application.validation.js";
+import { applyToBazaarSchema , getMyApplicationsSchema} from "./application.validation.js";
 import validate from "../../middlewares/validate.middleware.js";
 import role from "../../middlewares/role.middleware.js";
 import authMiddleware from "../../middlewares/auth.middleware.js";
+import validateQuery from "../../middlewares/validateQuery.middleware.js";
 
 const router = express.Router();
 const applicationController = new ApplicationController();
@@ -17,4 +18,11 @@ router.post(
   applicationController.applyToBazaar.bind(applicationController)
 );
 
+router.get(
+  "/me",
+  authMiddleware,
+  role(["vendor"]),
+  validateQuery(getMyApplicationsSchema), // Use the new middleware here
+  applicationController.getMyApplications.bind(applicationController)
+);
 export default router;
