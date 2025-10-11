ba4ec1a57ba849f3c9c629c10412c78c10f2436b
Walid Moussa <walidmoussa@gmail.com>
2025-10-11 20:47:57 +0300

    fix: update server config

--- server/index.ts
+++ server/index.ts
@@ -61,6 +61,6 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+    console.log("Server running on: http://localhost:5000");
   });
 })();
