df2526ad9bff37cb615029b8ea6e33f8b7add601
rawanhossam27 <rawanmansour2004@gmail.com>
2025-10-15 03:07:51 +0300

    changed app validation fro booth to export joi

--- server/src/features/applications/application.controller.js
+++ server/src/features/applications/application.controller.js
@@ -11,7 +11,7 @@ export class ApplicationController {
       const eventId = req.params.eventId; // Get eventId from the path
 
       // Validate input (body should NOT include event)
-      const { error } = validateBazaarApplication(req.body);
+      const { error } = validateBazaarApplication.validate(req.body);
       if (error)
         return res
           .status(400)
@@ -22,6 +22,7 @@ export class ApplicationController {
         vendorId,
         event: eventId, // Set event from path param
         type: "bazaar",
+        createdBy: user._id,
       };
 
       const newApplication = await ApplicationService.createApplication(
@@ -42,7 +43,7 @@ export class ApplicationController {
     try {
       const vendorId = req.user._id;
       // Validate input
-      const { error } = validateBoothApplication(req.body);
+      const { error } = validateBoothApplication.validate(req.body);
       if (error)
         return res
           .status(400)
