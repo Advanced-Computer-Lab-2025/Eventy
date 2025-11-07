d55ac2d07978dfaf7a496d6895fccdd0262575a1
Jomana Mahmoud Tantawy <140981371+JomanaMahmoud@users.noreply.github.com>
2025-11-07 16:52:33 +0200

    feat(transactions): add transaction routes to the main router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,6 +7,7 @@ import applicationRoutes from "../features/applications/application.route.js";
 import facilityRoutes from "../features/facilities/facility.route.js";
 import notificationRoutes from "../features/notifications/notification.route.js";
 import uploadRoutes from "../features/upload/upload.route.js";
+import transactionRoutes from "../features/transactions/transaction.route.js";
 
 const PORT = process.env.PORT || 4000;
 const router = express.Router();
@@ -44,5 +45,6 @@ router.use("/facilities", facilityRoutes);
 router.use("/notifications", notificationRoutes);
 // upload routes
 router.use("/upload", uploadRoutes);
-
+//transaction routes
+router.use("/transactions",transactionRoutes);
 export default router;
