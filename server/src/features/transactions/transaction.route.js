c1fa037f352312dc2fe4e71273796f48ea53a8a9
Jomana Mahmoud Tantawy <140981371+JomanaMahmoud@users.noreply.github.com>
2025-11-07 16:52:23 +0200

    feat(transactions): implement transaction routes for payment processing

---
+++ server/src/features/transactions/transaction.route.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { TransactionController } from "./transaction.controller.js";
+import authMiddleware from "../../middlewares/auth.middleware.js";
+import validate from "../../middlewares/validate.middleware.js";
+import { payForEventBodySchema, payForEventParamsSchema, walletTopUpSchema} from "./transaction.validation.js";
+
+const router = express.Router();
+const transactionController = new TransactionController();
+
+/**
+ * @route   POST /api/transactions/pay/:eventId
+ * @desc    Pay for a workshop or trip
+ * @access  Student, Staff, TA, Professor
+ */
+router.post(
+  "/pay/:eventId",
+  authMiddleware,
+  validate(payForEventParamsSchema, "params"), // validate eventId in URL
+  validate(payForEventBodySchema, "body"), // validate paymentMethod in body
+  transactionController.payForEvent.bind(transactionController)
+);
+
+/**
+ * @route   POST /api/transactions/confirm
+ * @desc    Confirm Stripe payment
+ * @access  Public (Stripe webhook or frontend)
+ */
+router.post(
+  "/confirm",
+  transactionController.confirmStripePayment.bind(transactionController)
+);
+
+/**
+ * @route   POST /api/transactions/wallet/top-up
+ * @desc    Start wallet top-up via Stripe
+ * @access  Authenticated users
+ */
+router.post(
+  "/wallet/top-up",
+  authMiddleware,
+  validate(walletTopUpSchema, "body"),
+  transactionController.topUpWallet.bind(transactionController)
+);
+export default router;
